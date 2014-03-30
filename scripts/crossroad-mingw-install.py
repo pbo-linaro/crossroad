4ccf1e5cc488162ed8ff3e352452dc166fd87621
Jehan <jehan@girinstud.io>
2014-03-31 05:34:11 +1300

    crossroad-mingw-install: default repository to openSUSE_13.1.

--- scripts/crossroad-mingw-install.py
+++ scripts/crossroad-mingw-install.py
@@ -467,7 +467,7 @@ def GetOptions():
 
   # Options specifiying download repository
   default_project = "windows:mingw:win32"
-  default_repository = "openSUSE_11.4"
+  default_repository = "openSUSE_13.1"
   default_repo_url = "http://download.opensuse.org/repositories/PROJECT/REPOSITORY/"
   repoOptions = OptionGroup(parser, "Specify download repository")
   repoOptions.add_option("-p", "--project", dest="project", default=default_project,
