056d9496c0a4a972d0b173af06a861083748196b
Jehan <jehan@girinstud.io>
2016-05-20 19:19:53 +0200

    mingw: update the dependency repository to SUSE Leap 42.1.
    Seems they made a jump in versionning from 13.2 to Leap 42.1.

--- scripts/crossroad-mingw-install.py
+++ scripts/crossroad-mingw-install.py
@@ -476,7 +476,7 @@ def GetOptions():
 
   # Options specifiying download repository
   default_project = "windows:mingw:win32"
-  default_repository = "openSUSE_13.2"
+  default_repository = "openSUSE_42.1"
   default_repo_url = "http://download.opensuse.org/repositories/PROJECT/REPOSITORY/"
   repoOptions = OptionGroup(parser, "Specify download repository")
   repoOptions.add_option("-p", "--project", dest="project", default=default_project,
