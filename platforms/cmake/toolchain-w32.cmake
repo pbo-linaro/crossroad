8d87e61f6124fcc42dbc9f68d8b3da0ab9f70bbc
Jehan <jehan@girinstud.io>
2017-12-24 19:22:00 +0100

    platforms: set CMAKE_SYSTEM_PROCESSOR for Win32 platforms.
    CMake wiki indicates that this is not set automatically when
    cross-compiling: https://cmake.org/Wiki/CMake_Cross_Compiling
    Yet this can be necessary on some CMake scripts (I use it in uchardet
    for instance).
    CMake docs also indicates that this value would be set to the
    PROCESSOR_ARCHITECTURE environment variable in Windows:
    https://cmake.org/cmake/help/v2.8.8/cmake.html#variable:CMAKE_SYSTEM_PROCESSOR
    Finally I could find that this variable usually takes "x86" or "AMD64"
    values for 32 and 64-bit targets respectively. So I'll just reproduce
    these values:
    https://blogs.msdn.microsoft.com/david.wang/2006/03/27/howto-detect-process-bitness/

--- platforms/cmake/toolchain-w32.cmake
+++ platforms/cmake/toolchain-w32.cmake
@@ -6,6 +6,13 @@ SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
+# This value is not set automatically when cross-compiling with CMake.
+# According to CMake doc, it would usually be set to the value of
+# PROCESSOR_ARCHITECTURE environment variable when natively compiling on
+# win32. Finally according to information I could find, this variable
+# would be "x86" for 32-bit or "AMD64" for 64-bit architecture.
+SET(CMAKE_SYSTEM_PROCESSOR x86)
+
 # pkg-config for searching packages for the target.
 SET(PKG_CONFIG_EXECUTABLE i686-w64-mingw32-pkg-config)
 
