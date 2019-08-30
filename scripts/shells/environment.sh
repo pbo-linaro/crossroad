647706f08a8ceff777950caee8a11fae5a7f7416
Jehan <jehan@girinstud.io>
2019-08-30 12:10:55 +0200

    scripts: also look in word-size specific folder for PYTHONPATH.
    For instance PyGObject python files were installed under lib64/.

--- scripts/shells/environment.sh
+++ scripts/shells/environment.sh
@@ -109,7 +109,7 @@ mkdir -p $CROSSROAD_PREFIX/include
 mkdir -p $CROSSROAD_PREFIX/lib
 
 # So that the system-wide python can still find any locale lib.
-for dir in $(find $CROSSROAD_PREFIX/lib/ -maxdepth 1 -name 'python3.*');
+for dir in $(find $CROSSROAD_PREFIX/lib/ $CROSSROAD_PREFIX/lib${CROSSROAD_WORD_SIZE}/ -maxdepth 1 -name 'python3.*');
 do
     export PYTHONPATH=:${dir}:$PYTHONPATH
 done;
