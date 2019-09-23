171561d1137dacc76adf2d0ecb6d60a025e0ee0e
Jehan <jehan@girinstud.io>
2019-09-23 02:44:46 +0200

    scripts: throw stderr to /dev/null in potentially broken `find`.
    This fixes such warning when starting the crossroad environment:
    > /ze/crossroad/prefix/lib64/’: No such file or directory
    
    Here for instance because there is no lib${CROSSROAD_WORD_SIZE}/
    directory.

--- scripts/shells/environment.sh
+++ scripts/shells/environment.sh
@@ -128,9 +128,9 @@ mkdir -p $CROSSROAD_PREFIX/include
 mkdir -p $CROSSROAD_PREFIX/lib
 
 # So that the system-wide python can still find any locale lib.
-for dir in $(find $CROSSROAD_PREFIX/lib/ $CROSSROAD_PREFIX/lib${CROSSROAD_WORD_SIZE}/ -maxdepth 1 -name 'python3.*');
+for dir in $(find $CROSSROAD_PREFIX/lib/ $CROSSROAD_PREFIX/lib${CROSSROAD_WORD_SIZE}/ -maxdepth 1 -name 'python3.*' 2>/dev/null);
 do
-    export PYTHONPATH=${dir}/site-packages:$PYTHONPATH
+  export PYTHONPATH=${dir}/site-packages:$PYTHONPATH
 done;
 # For gdbus-codegen.
 export PYTHONPATH=$CROSSROAD_PREFIX/share/glib-2.0/:$PYTHONPATH
